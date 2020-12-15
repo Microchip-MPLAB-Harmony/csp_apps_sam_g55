6ac33787d97894068eb45e353e3e2eeb4228af8d
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-12-16 01:03:16 +0530

    [APPS] Added FLEXCOM SPI Slave application flexcom_spi_write_read
    MH3-44573

---
+++ apps/flexcom/spi/slave/flexcom_spi_write_read/firmware/src/config/sam_g55_xpro/peripheral/flexcom/spi/slave/plib_flexcom5_spi_slave.c
@@ -0,0 +1,259 @@
+/*******************************************************************************
+  SPIFLEXCOM5 SPI PLIB
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_flexcom5_spi_slave.c
+
+  Summary:
+    SPIFLEXCOM5 SPI Slave PLIB Implementation File.
+
+  Description:
+    This file defines the interface to the FLEXCOM SPI peripheral library.
+    This library provides access to and control of the associated
+    peripheral instance.
+
+  Remarks:
+    None.
+
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+#include "plib_flexcom5_spi_slave.h"
+#include "peripheral/pio/plib_pio.h"
+#include <string.h>
+
+#define FLEXCOM5_READ_BUFFER_SIZE            256
+#define FLEXCOM5_WRITE_BUFFER_SIZE           256
+
+static uint8_t FLEXCOM5_ReadBuffer[FLEXCOM5_READ_BUFFER_SIZE];
+static uint8_t FLEXCOM5_WriteBuffer[FLEXCOM5_WRITE_BUFFER_SIZE];
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: SPIFLEXCOM5 SPI Implementation
+// *****************************************************************************
+// *****************************************************************************
+
+/* Global object to save FLEXCOM SPI Exchange related data */
+FLEXCOM_SPI_SLAVE_OBJECT flexcom5SpiObj;
+
+void FLEXCOM5_SPI_Initialize( void )
+{
+    /* Set FLEXCOM SPI operating mode */
+    FLEXCOM5_REGS->FLEXCOM_MR = FLEXCOM_MR_OPMODE_SPI;
+
+    /* Disable and Reset the SPI*/
+    SPI5_REGS->SPI_CR = SPI_CR_SPIDIS_Msk | SPI_CR_SWRST_Msk;
+
+    /* SPI is by default in Slave Mode, disable mode fault detection */
+    SPI5_REGS->SPI_MR = SPI_MR_MODFDIS_Msk;
+
+    /* Set up clock Polarity, data phase, Communication Width */
+    SPI5_REGS->SPI_CSR[0] = SPI_CSR_CPOL(0) | SPI_CSR_NCPHA(1) | SPI_CSR_BITS_8_BIT;
+
+    flexcom5SpiObj.rdInIndex = 0;
+    flexcom5SpiObj.wrOutIndex = 0;
+    flexcom5SpiObj.nWrBytes = 0;
+    flexcom5SpiObj.errorStatus = FLEXCOM_SPI_SLAVE_ERROR_NONE;
+    flexcom5SpiObj.callback = NULL ;
+    flexcom5SpiObj.transferIsBusy = false ;
+
+    /* Set the Busy Pin to ready state */
+    PIO_PinWrite((PIO_PIN)PIO_PIN_PA24, 0);
+
+    /* Enable Receive full and chip deselect interrupt */
+    SPI5_REGS->SPI_IER = (SPI_IER_RDRF_Msk | SPI_IER_NSSR_Msk);
+
+    /* Enable SPIFLEXCOM5 */
+    SPI5_REGS->SPI_CR = SPI_CR_SPIEN_Msk;
+}
+
+/* For 9-bit mode, the "size" must be specified in terms of 16-bit words */
+size_t FLEXCOM5_SPI_Read(void* pRdBuffer, size_t size)
+{
+    size_t rdSize = size;
+    size_t rdInIndex = flexcom5SpiObj.rdInIndex;
+
+    if (rdSize > rdInIndex)
+    {
+        rdSize = rdInIndex;
+    }
+
+    memcpy(pRdBuffer, FLEXCOM5_ReadBuffer, rdSize);
+
+    return rdSize;
+}
+
+/* For 9-bit mode, the "size" must be specified in terms of 16-bit words */
+size_t FLEXCOM5_SPI_Write(void* pWrBuffer, size_t size )
+{
+    uint32_t intState = SPI5_REGS->SPI_IMR;
+    size_t wrSize = size;
+
+    SPI5_REGS->SPI_IDR = intState;
+
+    if (wrSize > FLEXCOM5_WRITE_BUFFER_SIZE)
+    {
+        wrSize = FLEXCOM5_WRITE_BUFFER_SIZE;
+    }
+
+    memcpy(FLEXCOM5_WriteBuffer, pWrBuffer, wrSize);
+
+    flexcom5SpiObj.nWrBytes = wrSize;
+    flexcom5SpiObj.wrOutIndex = 0;
+
+    while ((SPI5_REGS->SPI_SR & SPI_SR_TDRE_Msk) && (flexcom5SpiObj.wrOutIndex < flexcom5SpiObj.nWrBytes))
+    {
+        *((uint8_t*)&SPI5_REGS->SPI_TDR) = FLEXCOM5_WriteBuffer[flexcom5SpiObj.wrOutIndex++];
+    }
+
+    /* Restore interrupt enable state and also enable TDRE interrupt */
+    SPI5_REGS->SPI_IER = (intState | SPI_IER_TDRE_Msk);
+
+    return wrSize;
+}
+
+/* For 9-bit mode, the return value is in terms of 16-bit words */
+size_t FLEXCOM5_SPI_ReadCountGet(void)
+{
+    return flexcom5SpiObj.rdInIndex;
+}
+
+/* For 9-bit mode, the return value is in terms of 16-bit words */
+size_t FLEXCOM5_SPI_ReadBufferSizeGet(void)
+{
+    return FLEXCOM5_READ_BUFFER_SIZE;
+}
+
+/* For 9-bit mode, the return value is in terms of 16-bit words */
+size_t FLEXCOM5_SPI_WriteBufferSizeGet(void)
+{
+    return FLEXCOM5_WRITE_BUFFER_SIZE;
+}
+
+void FLEXCOM5_SPI_CallbackRegister(FLEXCOM_SPI_SLAVE_CALLBACK callBack, uintptr_t context )
+{
+    flexcom5SpiObj.callback = callBack;
+
+    flexcom5SpiObj.context = context;
+}
+
+/* The status is returned busy during the period the chip-select remains asserted */
+bool FLEXCOM5_SPI_IsBusy(void)
+{
+    return flexcom5SpiObj.transferIsBusy;
+}
+
+/* Drive the GPIO pin to indicate to SPI Master that the slave is ready now */
+void FLEXCOM5_SPI_Ready(void)
+{
+    PIO_PinWrite((PIO_PIN)PIO_PIN_PA24, 0);
+}
+
+FLEXCOM_SPI_SLAVE_ERROR FLEXCOM5_SPI_ErrorGet(void)
+{
+    FLEXCOM_SPI_SLAVE_ERROR errorStatus = flexcom5SpiObj.errorStatus;
+
+    flexcom5SpiObj.errorStatus = FLEXCOM_SPI_SLAVE_ERROR_NONE;
+
+    return errorStatus;
+}
+
+void FLEXCOM5_InterruptHandler(void)
+{
+    uint8_t txRxData = 0;
+
+    uint32_t statusFlags = SPI5_REGS->SPI_SR;
+
+    if (statusFlags & SPI_SR_OVRES_Msk)
+    {
+        /*OVRES flag is cleared on reading SPI SR*/
+
+        /* Save the error to report it to application later */
+        flexcom5SpiObj.errorStatus = SPI_SR_OVRES_Msk;
+    }
+
+    if(statusFlags & SPI_SR_RDRF_Msk)
+    {
+        if (flexcom5SpiObj.transferIsBusy == false)
+        {
+            flexcom5SpiObj.transferIsBusy = true;
+
+            PIO_PinWrite((PIO_PIN)PIO_PIN_PA24, 1);
+        }
+
+        while ((statusFlags |= SPI5_REGS->SPI_SR) & SPI_SR_RDRF_Msk)
+        {
+            /* Reading DATA register will also clear the RDRF flag */
+            txRxData = *((uint8_t*)&SPI5_REGS->SPI_RDR);
+
+            if (flexcom5SpiObj.rdInIndex < FLEXCOM5_READ_BUFFER_SIZE)
+            {
+                FLEXCOM5_ReadBuffer[flexcom5SpiObj.rdInIndex++] = txRxData;
+            }
+
+            statusFlags &= ~SPI_SR_RDRF_Msk;
+        }
+    }
+
+    if(statusFlags & SPI_SR_TDRE_Msk)
+    {
+        while (((statusFlags |= SPI5_REGS->SPI_SR) & SPI_SR_TDRE_Msk) && (flexcom5SpiObj.wrOutIndex < flexcom5SpiObj.nWrBytes))
+        {
+            *((uint8_t*)&SPI5_REGS->SPI_TDR) = FLEXCOM5_WriteBuffer[flexcom5SpiObj.wrOutIndex++];
+            statusFlags &= ~SPI_SR_TDRE_Msk;
+        }
+
+        if (flexcom5SpiObj.wrOutIndex >= flexcom5SpiObj.nWrBytes)
+        {
+            /* Disable TDRE interrupt. The last byte sent by the master will be shifted out automatically */
+            SPI5_REGS->SPI_IDR = SPI_IDR_TDRE_Msk;
+        }
+    }
+
+    if(statusFlags & SPI_SR_NSSR_Msk)
+    {
+        /* NSSR flag is cleared on reading SPI SR */
+
+        flexcom5SpiObj.transferIsBusy = false;
+
+        flexcom5SpiObj.wrOutIndex = 0;
+        flexcom5SpiObj.nWrBytes = 0;
+
+        if(flexcom5SpiObj.callback != NULL)
+        {
+            flexcom5SpiObj.callback(flexcom5SpiObj.context);
+        }
+
+        /* Clear the rdInIndex. Application must read the received data in the callback. */
+        flexcom5SpiObj.rdInIndex = 0;
+    }
+}
