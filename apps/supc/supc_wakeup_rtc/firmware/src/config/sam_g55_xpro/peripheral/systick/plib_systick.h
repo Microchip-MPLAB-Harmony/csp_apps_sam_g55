aa4330d9073a240bdd5b090ea3a810ddc3070d49
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-12 01:00:10 +0530

    [SUPC] Regenerated demo for supc_supc_wakeup_rtc_sam_g55_xpro
    MH3-68002
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/supc/supc_wakeup_rtc/firmware/src/config/sam_g55_xpro/peripheral/systick/plib_systick.h
+++ apps/supc/supc_wakeup_rtc/firmware/src/config/sam_g55_xpro/peripheral/systick/plib_systick.h
@@ -55,11 +55,18 @@
 // *****************************************************************************
 // *****************************************************************************
 
-#define SYSTICK_FREQ   119996416U
+#define SYSTICK_FREQ   100007936U
 
 #define SYSTICK_INTERRUPT_PERIOD_IN_US  (100000U)
 
 typedef void (*SYSTICK_CALLBACK)(uintptr_t context);
+
+
+typedef struct
+{ 
+    uint32_t start; 
+    uint32_t count; 
+}SYSTICK_TIMEOUT;
 
 typedef struct
 {
@@ -80,6 +87,10 @@ void SYSTICK_DelayMs ( uint32_t delay_ms );
 void SYSTICK_DelayUs ( uint32_t delay_us );
 
 void SYSTICK_TimerCallbackSet ( SYSTICK_CALLBACK callback, uintptr_t context );
+uint32_t SYSTICK_GetTickCounter(void);
+void SYSTICK_StartTimeOut (SYSTICK_TIMEOUT* timeout, uint32_t delay_ms);
+void SYSTICK_ResetTimeOut (SYSTICK_TIMEOUT* timeout);
+bool SYSTICK_IsTimeoutReached (SYSTICK_TIMEOUT* timeout);
 #ifdef __cplusplus // Provide C++ Compatibility
  }
 #endif
